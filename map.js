640571245bd2da45d09535733b78fa0847586ded
Radek <plocharadoslaw@gmail.com>
2023-05-08 10:46:34 +0200

    leetcode js 30 day challange day 4 - apply transfrom on each arr element without map

---
+++ map.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function (arr, fn) {
+  const resArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    resArr.push(fn(arr[i], i));
+  }
+  return resArr;
+};
